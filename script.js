cfb06c023c3aa8764b11ebbf2318a53c874be917
MehdiFav <mehdi.favreau@gmail.com>
2022-07-15 19:13:14 +0200

    Script bouton Roll et Hold + insertion bon png du dé correspondant au roll

--- script.js
+++ script.js
@@ -1,7 +1,3 @@
-/*
-* commencer par un seul player
-* gestion score instantané et global du player
-*/
 
 let newGame = document.getElementById("newGame");
 let diceRoll = document.getElementById("rollButton");
@@ -12,8 +8,11 @@ let scoreP2 = document.getElementById("scoreP2");
 let currentScoreP1 = document.getElementById("currentScoreP1");
 let currentScoreP2 = document.getElementById("currentScoreP2");
 let zero = 0;
+let updateCurrentScore = zero;
+let globalScoreP1 = zero;
 
-//Méthode edition noms, initialisation partie
+
+//Méthode edition des noms, initialisation partie
 newGame.addEventListener("click", function() {
     resetScores();
     let player1 = prompt("Entrez le nom du joueur 1");
@@ -21,8 +20,24 @@ newGame.addEventListener("click", function() {
 
     document.getElementById("playerName1").innerHTML = player1;
     document.getElementById("playerName2").innerHTML = player2;
+    console.log(`Vous avez démarré une nouvelle partie avec les joueurs : ${player1} et ${player2} !`);
 });
 
+diceRoll.addEventListener("click", function() {
+    var numberDice = Math.floor(Math.random() * 6) + 1;
+    showDice(numberDice);
+    updateCurrentScore += numberDice ;
+    currentScoreP1.innerHTML = updateCurrentScore;
+});
+
+hold.addEventListener("click", function() {
+    globalScoreP1 += updateCurrentScore;
+    scoreP1.innerHTML = globalScoreP1;
+    updateCurrentScore = zero;
+    currentScoreP1.innerHTML = zero;
+});
+
+//reset des scores
 function resetScores() {
     scoreP1.innerHTML = zero;
     scoreP2.innerHTML = zero;
@@ -30,4 +45,8 @@ function resetScores() {
     currentScoreP2.innerHTML = zero;
 }
 
+//Affiche les bonnes faces de dé
+function showDice(dice) {
+    diceFace.src = 'img/face-' + dice + '.png';
+}
 
