21d90d2a6060a3d58106caace5fecf1a984eda1d
MehdiFav <mehdi.favreau@gmail.com>
2022-07-15 17:27:20 +0200

    Début script JS bouton New Game

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
+/*
+* commencer par un seul player
+* gestion score instantané et global du player
+*/
 
-//commencer par un seul player
-//gestion score instantané et global du player
+let newGame = document.getElementById("newGame");
+let diceRoll = document.getElementById("rollButton");
+let hold = document.getElementById("holdButton");
+let diceFace = document.querySelector(".dice");
+let scoreP1 = document.getElementById("scoreP1");
+let scoreP2 = document.getElementById("scoreP2");
+let currentScoreP1 = document.getElementById("currentScoreP1");
+let currentScoreP2 = document.getElementById("currentScoreP2");
+let zero = 0;
+
+//Méthode edition noms, initialisation partie
+newGame.addEventListener("click", function() {
+    resetScores();
+    let player1 = prompt("Entrez le nom du joueur 1");
+    let player2 = prompt("Entrez le nom du joueur 2");
+
+    document.getElementById("playerName1").innerHTML = player1;
+    document.getElementById("playerName2").innerHTML = player2;
+});
+
+function resetScores() {
+    scoreP1.innerHTML = zero;
+    scoreP2.innerHTML = zero;
+    currentScoreP1.innerHTML = zero;
+    currentScoreP2.innerHTML = zero;
+}
+
 
