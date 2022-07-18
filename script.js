306d8d4c8398e75e56aa2b9423f7e6736da58d07
MehdiFav <mehdi.favreau@gmail.com>
2022-07-18 14:17:55 +0200

    Intégration de la fonction switchPLayer dans le script

--- script.js
+++ script.js
@@ -10,13 +10,17 @@ let currentScoreP2 = document.getElementById("currentScoreP2");
 let zero = 0;
 let updateCurrentScore = zero;
 let globalScoreP1 = zero;
+let globalScoreP2 = zero;
+let player = 1;
+let player1;
+let player2;
 
 
 //Méthode edition des noms, initialisation partie
 newGame.addEventListener("click", function() {
     resetScores();
-    let player1 = prompt("Entrez le nom du joueur 1");
-    let player2 = prompt("Entrez le nom du joueur 2");
+    player1 = prompt("Entrez le nom du joueur 1");
+    player2 = prompt("Entrez le nom du joueur 2");
 
     document.getElementById("playerName1").innerHTML = player1;
     document.getElementById("playerName2").innerHTML = player2;
@@ -24,23 +28,45 @@ newGame.addEventListener("click", function() {
 });
 
 diceRoll.addEventListener("click", function() {
-    var numberDice = Math.floor(Math.random() * 6) + 1;
+    let numberDice = Math.floor(Math.random() * 6) + 1;
     showDice(numberDice);
 
     if(numberDice !== 1) {
         updateCurrentScore += numberDice ;
-        currentScoreP1.innerHTML = updateCurrentScore;
+        document.getElementById("currentScoreP" + player).innerHTML = updateCurrentScore;   
     } else {
         updateCurrentScore = zero;
-        currentScoreP1.innerHTML = updateCurrentScore;
+        document.getElementById("currentScoreP" + player).innerHTML = updateCurrentScore;
+        switchPlayer(player);
     }
 });
 
 hold.addEventListener("click", function() {
-    globalScoreP1 += updateCurrentScore;
-    scoreP1.innerHTML = globalScoreP1;
+    if(player == 1) {
+       globalScoreP1 += updateCurrentScore;
+       document.getElementById("scoreP" + player).innerHTML = globalScoreP1; 
+    } else {
+       globalScoreP2 += updateCurrentScore;
+       document.getElementById("scoreP" + player).innerHTML = globalScoreP2;  
+    }
+    
     updateCurrentScore = zero;
-    currentScoreP1.innerHTML = zero;
+    document.getElementById("currentScoreP" + player).innerHTML = zero;
+    
+    if(player == 1){
+        if(globalScoreP1 >= 100){
+            alert(`${player1} a gagné !!!!`);
+        } else {
+            switchPlayer(player);
+        }
+    } else {
+       if(globalScoreP2 >= 100){
+            alert(`${player2} a gagné !!!!`);
+        } else {
+            switchPlayer(player);
+        }
+    }
+    
 });
 
 //reset des scores
@@ -49,6 +75,10 @@ function resetScores() {
     scoreP2.innerHTML = zero;
     currentScoreP1.innerHTML = zero;
     currentScoreP2.innerHTML = zero;
+    updateCurrentScore = zero;
+    globalScoreP1 = zero;
+    globalScoreP2 = zero;
+    player = 1;
 }
 
 //Affiche les bonnes faces de dé
@@ -56,3 +86,10 @@ function showDice(dice) {
     diceFace.src = 'img/face-' + dice + '.png';
 }
 
+function switchPlayer(nbPlayer) {
+    if(player == 1) {
+        player = 2;
+    } else {
+        player = 1;
+    }
+}
